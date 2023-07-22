5c8c03be6a1b8816bc09a4b83657aa97e9de1704
admin <aasthabarve0@gmail.com>
2023-07-22 13:58:16 +0530

    calculate wage for month by condition..

--- src/com/breidgelabs/employeewage/EmployeeWageComputation.java
+++ src/com/breidgelabs/employeewage/EmployeeWageComputation.java
@@ -2,36 +2,58 @@ package com.breidgelabs.employeewage;
 
 public class EmployeeWageComputation {
 
-        static int finalWage = 0;
-        public static void main (String[]args){
-            System.out.println("Employee Wage Calculation");
-            int totalWage = 0;
-            int perHourWage = 20;
-            int fullDayWorkHrs = 8;
-            int partTimeWorkHrs = 4;
-            int totalWorkingDaysInMonth = 20;
+    static int finalWage = 0;
+    static int finalWorkHrs = 0;
 
-            for (int i = 0; i < totalWorkingDaysInMonth; i++) {
-                int isPresent = (int) (Math.random() * 10) % 3;
-                switch (isPresent) {
-                    case 1:
-                        System.out.println("Emp is Full day present");
-                        totalWage = (fullDayWorkHrs * perHourWage);
-                        break;
-                    case 2:
-                        System.out.println("Emp doing part time");
-                        totalWage = (partTimeWorkHrs * perHourWage);
-                        break;
-                    case 0:
-                        System.out.println("Emp is absent");
-                        totalWage = 0;
-                        break;
-                    default:
-                        System.out.println("!!!Something wrong!!!");
-                        break;
-                }
-                finalWage = finalWage + totalWage;
-                System.out.println("FINAL WAGE = " + finalWage);
+    public static void main(String[] args) {
+        System.out.println("Employee Wage Calculation");
+        int totalWage = 0;
+        int perHourWage = 20;
+        int fullDayWorkHrs = 8;
+        int partTimeWorkHrs = 4;
+        int totalWorkingDaysInMonth = 20;
+        int empHrs = 0;
+
+        for (int i = 0; i < totalWorkingDaysInMonth; i++) {
+            int isPresent = (int) (Math.random() * 10) % 3;
+            switch (isPresent) {
+                case 1:
+                    System.out.println("Emp is Full day present");
+                    totalWage = (fullDayWorkHrs * perHourWage);
+                    empHrs = 8;
+                    break;
+                case 2:
+                    System.out.println("Emp doing part time");
+                    totalWage = (partTimeWorkHrs * perHourWage);
+                    empHrs = 4;
+                    break;
+                case 0:
+                    System.out.println("Emp is absent");
+                    totalWage = 0;
+                    empHrs = 0;
+                    break;
+                default:
+                    System.out.println("!!!Something wrong!!!");
+                    break;
+            }
+            finalWage = finalWage + totalWage;
+            System.out.println("FINAL WAGE = " + finalWage);
+
+            finalWorkHrs = finalWorkHrs + empHrs;
+            System.out.println("FINAL WORK HRS = " + finalWorkHrs);
+
+            if (finalWorkHrs == 100) {
+                System.out.println("EMP COMPLETED HIS WORK HRS OF THIS MONTH");
+                break;
+            } else if (finalWorkHrs > 100) {
+                System.out.println("!Work Hrs Exceeds the limit! Reset");
+                finalWage = finalWage - totalWage;
+                System.out.println("Final wage --> " + finalWage);
+
+                finalWorkHrs = finalWorkHrs - empHrs;
+                System.out.println("Final work hrs --> " + finalWorkHrs);
             }
         }
+        System.out.println("FINAL WAGE = " + finalWage);
     }
+}
