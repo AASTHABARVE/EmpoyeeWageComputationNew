dbb2d162ff70e896c713b4743e8ba15a4c633a21
admin <aasthabarve0@gmail.com>
2023-07-22 12:03:55 +0530

    Employee present or absent

--- src/com/breidgelabs/employeewage/EmployeeWageComputation.java
+++ src/com/breidgelabs/employeewage/EmployeeWageComputation.java
@@ -2,6 +2,12 @@ package com.breidgelabs.employeewage;
 
 public class EmployeeWageComputation {
     public static void main(String args[]) {
-        System.out.println("Welcome To Employee Wage");
+            System.out.println("Employee Wage Calculation");
+            int isPresent = (int)(Math.random() * 10)%2;
+            if(isPresent == 1){
+                System.out.println("Employee is Present");
+            }else{
+                System.out.println("Employee is Absent");
+            }
+        }
     }
-}
