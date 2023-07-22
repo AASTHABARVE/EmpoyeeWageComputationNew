302341ed28adfcc4a339f90f694d04ce35adeaef
admin <aasthabarve0@gmail.com>
2023-07-22 12:11:29 +0530

    Daily wage calculation

--- src/com/breidgelabs/employeewage/EmployeeWageComputation.java
+++ src/com/breidgelabs/employeewage/EmployeeWageComputation.java
@@ -2,12 +2,19 @@ package com.breidgelabs.employeewage;
 
 public class EmployeeWageComputation {
     public static void main(String args[]) {
-            System.out.println("Employee Wage Calculation");
-            int isPresent = (int)(Math.random() * 10)%2;
-            if(isPresent == 1){
-                System.out.println("Employee is Present");
-            }else{
-                System.out.println("Employee is Absent");
+        System.out.println("Daily Wage Calculation");
+                int totalWage = 0;
+                int perHourWage = 20;
+                int fullDayWorkHrs = 8;
+
+                int isPresent = (int) (Math.random() * 10) % 2;
+                if (isPresent == 1) {
+                    System.out.println("Employee is Present");
+                    totalWage = (fullDayWorkHrs * perHourWage);
+                } else {
+                    System.out.println("Employee is Absent");
+                    totalWage = 0;
+                }
+                System.out.println("TOTAL WAGE = " + totalWage);
             }
-        }
-    }
+}
